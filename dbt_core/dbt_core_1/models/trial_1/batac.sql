3eb8dbf32c2dc4dc5b5a6c0fe1a993be9f824178
jom_vm <johnjo555arubio@gmail.com>
2025-03-26 12:05:54 +0000

    trying batac

---
+++ dbt_core/dbt_core_1/models/trial_1/batac.sql
@@ -0,0 +1,11 @@
+
+WITH CTE1 AS (
+SELECT PILLAR_INDICATOR,`2023`
+FROM `dataengineeringproject-454111.test_dataset_kestra.Batac_raw`
+UNION ALL
+SELECT PILLAR_INDICATOR,`2023`
+FROM `dataengineeringproject-454111.test_dataset_kestra.Candon_raw`
+
+)
+
+select PILLAR_INDICATOR,`2023` from CTE1
