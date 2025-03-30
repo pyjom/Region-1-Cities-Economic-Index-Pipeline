17cfbb46b06351463583b192adc448af7eefb442
jom_vm <johnjo555arubio@gmail.com>
2025-03-30 12:24:08 +0000

    removing first vversion of dbt core

--- dbt_core/dbt_core_1/models/trial_1/jinja_code.sql
+++
@@ -1,9 +0,0 @@
-{# This Jinja code generates SELECT statements to print numbers    #}
-{% set max_number = 5 %}
-{% for i in range (max_number)%}
-
-SELECT {{i}} AS number 
-    {% if not loop.last %}
-    UNION
-    {% endif %}
-{% endfor %}    
